391a2fade5a1016e1b8c3ba056b1b6eadfe223ac
Rage <rage@rage.rage>
2026-03-20 21:57:03 +0000

    chore(settings): adicionar exemplo de configuração do database

---
+++ BuscaFast/BuscaFast/settings_db_exemple.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seu_db',
+        'USER': 'seu_user',
+        'PASSWORD': 'sua_senha',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+# Preencha essas informacoes para rodar o MySQL, tire o exemple do nome e importe
