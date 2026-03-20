f03af28fd54c8621f4125a1a404e2fd94a0fb997
Rage <rage@rage.rage>
2026-03-20 23:50:22 +0000

    chore(security): mover SECRET_KEY para arquivo seguro fora do versionamento

--- BuscaFast/BuscaFast/api_keys_exemple.py
+++ BuscaFast/BuscaFast/api_keys_exemple.py
@@ -1,4 +1,8 @@
-TWITCH_CLIENT_ID = "SEU_CLIENT_ID_AQUI"
-TWITCH_CLIENT_SECRET = "SEU_CLIENT_SECRET_AQUI"
-
 # https://api-docs.igdb.com/#account-creation
+
+TWITCH_CLIENT_ID = "SEU_CLIENT_ID_AQUI" # coloca o seu id aqui 
+TWITCH_CLIENT_SECRET = "SEU_CLIENT_SECRET_AQUI" # gera seu token secreto e cola aqui
+
+# DJANGO: 
+
+SECRET_KEY = "cole_aqui_a_chave_gerada" # Gera uma secret_key django e cola aqui
