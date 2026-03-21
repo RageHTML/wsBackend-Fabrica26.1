7c535491adaf07102313be38974f786ea5805914
Rage <rage@rage.rage>
2026-03-21 21:42:39 +0000

    feat: adiciona rota remove_from_list em urls.py

--- BuscaFast/games/urls.py
+++ BuscaFast/games/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import search_page,list_page,add_to_list
+from .views import search_page,list_page,add_to_list,remove_to_list
 
 urlpatterns = [
     path("buscar_jogos/", search_page, name="search_page"), # "buscar_jogos" e a rota, search_page func de view, name e para chamar no html 
     path("minha_lista/", list_page, name="list_page"), # pagina mostrando lista de jogos favoritos
     path("add_to_list/", add_to_list, name="add_to_list"), # rota para enviar game_name para add_to_list
+    path("remove_to_list/", remove_to_list, name="remove_to_list")
 ]
