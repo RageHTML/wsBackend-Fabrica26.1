62fd8c9e7b859f079c95a4a789cd6e5233e76383
Rage <rage@rage.rage>
2026-03-21 18:49:38 +0000

    views: função save_from_api adicionada para salvar automaticamente no DB sempre que pesquisar um jogo

--- BuscaFast/games/views.py
+++ BuscaFast/games/views.py
@@ -2,20 +2,6 @@ from django.shortcuts import render
 from BuscaFast.services import get_game_by_name
 from .models import Game
 
-def search_page(request):
-    # lidando com dados invalidos 
-    query = request.GET.get("game_name","") # coloca o id do form de search.html, para nao retorna none...
-    query = query.strip() # removendo espacos fim/inicio
-    query = query.replace('"','') # removendo aspas duplas
-    query = query.replace("'",'') # removendo aspas simples
-    results = "none" # definindo a variavel vazia por padrao
-    if query:
-        results = get_game_by_name(query) # enviando o dado ja tratado para get_game_by_name, e armazenando na variavel results
-    return render(request, "games/search.html", {"results": results,"query": query}) # request e a requisicao, games/search.html e o template passando o contexto {query,results}
-
-def list_page(request):
-
-    return render(request, "games/list.html")
 
 def save_from_api(igdb_id,name): # salvar jogos da api no db Game, sempre que alguemm pesquisar (para gerar insights futuros)
     data = get_game_by_name(name)
@@ -33,3 +19,25 @@ def save_from_api(igdb_id,name): # salvar jogos da api no db Game, sempre que al
         }
     )
     return game
+
+def search_page(request):
+    # lidando com dados invalidos 
+    query = request.GET.get("game_name","") # coloca o id do form de search.html, para nao retorna none...
+    query = query.strip() # removendo espacos fim/inicio
+    query = query.replace('"','') # removendo aspas duplas
+    query = query.replace("'",'') # removendo aspas simples
+    results = "none" # definindo a variavel vazia por padrao
+    if query:
+        data = get_game_by_name(query) # salva a tabela do jogo pesquisado da api  
+        results = [] # cria uma tabela para guardar o resultado
+        for item in data: # iterando dentro da data para salvar o bd
+            game = save_from_api(item["id"],item["name"]) # retorna os dados depois de armazena na db
+            if game:
+                results.append(game) # adicionando na lista results
+
+
+    return render(request, "games/search.html", {"results": results,"query": query}) # request e a requisicao, games/search.html e o template passando o contexto {query,results}
+
+def list_page(request):
+
+    return render(request, "games/list.html")
