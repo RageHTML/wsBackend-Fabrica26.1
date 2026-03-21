66cfdabf3e5e0745e2bb1a0a6384d30fd9a03880
Rage <rage@rage.rage>
2026-03-21 21:09:18 +0000

    fix: refatora funções de token e adição de jogos à lista do usuário

--- BuscaFast/games/views.py
+++ BuscaFast/games/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from BuscaFast.services import get_game_by_name
 import uuid
-from .models import Game
+from .models import Game, UserGameList
 
-
+# BUSCAR_JOGOS 
 def save_from_api(igdb_id,name): # salvar jogos da api no db Game, sempre que alguemm pesquisar (para gerar insights futuros)
     data = get_game_by_name(name)
     if not data or len(data) == 0: # verificar se a data nao existe ou esta vazia, retorna none
@@ -42,9 +42,37 @@ def search_page(request):
 
     return render(request, "games/search.html", {"results": results,"query": query}) # request e a requisicao, games/search.html e o template passando o contexto {query,results}
 
-def list_page(request):
+# Minha_Lista
+def get_token(request): # Obter token ou gerar
     token = request.session.get("user_token") # verificar se o usuario ja tem token
     if not token: # gerar um token para o novo usuario
         token = str(uuid.uuid4())
         request.session["user_token"] = token
-    return render(request, "games/list.html", {"user_token": token})
+    return token
+   
+def add_to_list(request): # adiciona Jogo a lista
+    token = get_token(request) # obter token unico
+    game_name = request.GET.get("game_name") # recebendo o nome do jogo enviado de search.html
+    game = Game.objects.filter(name=game_name).first() # procurando no bd(Game) pelo game_name
+
+    if not game and game_name: # caso nao ache no bd(Game)
+        data = get_game_by_name(game_name) # Busca na api em services
+        if data: 
+            api_game = data[0] 
+            game = save_from_api(api_game["id"],api_game["name"]) # salvar no bd(Game)
+    
+    if game: # quando estiver com o game, salvar no bd (UserGamerList)
+        user_list, created = UserGameList.objects.get_or_create(user_token=token) # retorna uma tupla(user_list,created)
+        user_list.games.add(game) # add por causa que o campo games e ManyToManyFields
+
+    return redirect("list_page")
+
+ 
+def list_page(request):
+    token = get_token(request) # obter token 
+
+    user_list, created = UserGameList.objects.get_or_create(user_token=token) # buscar a lista com o id do usuario
+    games = user_list.games.all() # pegar todos os jogos da lista 
+
+    return render(request, "games/list.html", {"user_token": token, "games": games})
+
