0ed550bfa555319c9707b62d6721cad147540408
Rage <rage@rage.rage>
2026-03-21 19:01:43 +0000

    refactor(models): renomeando igbd_id para igdb_id

--- BuscaFast/games/models.py
+++ BuscaFast/games/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Game(models.Model): # Avisando ao DB que vou criar uma tabela chamada Game
-    igbd_id = models.IntegerField(unique=True) # Id vindo da api, nao pode repitir 
+    igdb_id = models.IntegerField(unique=True) # Id vindo da api, nao pode repitir 
     name = models.CharField(max_length=250) # nome do jogo, limute maximo de 250 caracteres
     rating = models.FloatField(null=True,blank=True) # nota do jogo, numero decimal, pode ser nulo(nem todo jogo tem nota)
     cover_url = models.CharField(max_length=500, null=True, blank=True) # armazena a url da imagem
