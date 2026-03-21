09a347f0e1832a5880149f546cce0ff8a6b54dd2
Rage <rage@rage.rage>
2026-03-21 18:04:45 +0000

    adicionada página list.html, configuradas views e urls para carregar a página de listas

--- BuscaFast/games/urls.py
+++ BuscaFast/games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import search_page
+from .views import search_page,list_page
 
 urlpatterns = [
     path("buscar_jogos/", search_page, name="search_page"), # "buscar_jogos" e a rota, search_page func de view, name e para chamar no html 
+    path("minha_lista/", list_page, name="list_page"), 
 ]
--- BuscaFast/games/views.py
+++ BuscaFast/games/views.py
@@ -3,7 +3,7 @@ from BuscaFast.services import get_game_by_name
 
 def search_page(request):
     # lidando com dados invalidos 
-    query = request.GET.get("game_name","") # coloca o id do form de search.html, para nao ter problemas...
+    query = request.GET.get("game_name","") # coloca o id do form de search.html, para nao retorna none...
     query = query.strip() # removendo espacos fim/inicio
     query = query.replace('"','') # removendo aspas duplas
     query = query.replace("'",'') # removendo aspas simples
@@ -11,3 +11,6 @@ def search_page(request):
     if query:
         results = get_game_by_name(query) # enviando o dado ja tratado para get_game_by_name, e armazenando na variavel results
     return render(request, "games/search.html", {"results": results,"query": query}) # request e a requisicao, games/search.html e o template passando o contexto {query,results}
+
+def list_page(request):
+    return render(request, "games/list.html")
