1c93f3eea873cf899ef720e0aeff2b3b832ef657
Rage <rage@rage.rage>
2026-03-21 15:44:31 +0000

    fix(services): corrigir formatação do body usando aspas simples e encoding utf-8

--- BuscaFast/BuscaFast/services.py
+++ BuscaFast/BuscaFast/services.py
@@ -23,9 +23,9 @@ def get_game_by_name(name): # buscar dados de um jogo pelo o nome
         "Authorization": f"Bearer {token}" # prova que voce esta autheticado
     }
 
-    body = f"fields id,name,rating; search'{name}'; limit 1;" # Query de busca (tipo GraphQL), # fields sao os dados que eu quero, search e onde o parametro vai ser inserido, limit traz apenas 1 dado 
+    body = f'fields id,name,rating; search"{name}"; limit 1;' # Query de busca (tipo GraphQL), # fields sao os dados que eu quero, search e onde o parametro vai ser inserido, limit traz apenas 1 dado 
 
-    response = requests.post(url,headers=headers, data=body) # metodo post (apontando para o caminho url, passando o headers, body)
+    response = requests.post(url,headers=headers, data=body.encode('utf-8')) # metodo post (apontando para o caminho url, passando o headers, body)
 
     return response.json() # converte a resposta da api (dict/list)
 
